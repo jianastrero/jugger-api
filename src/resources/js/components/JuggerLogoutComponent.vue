35d016a0869bde2491d0006bff2ef46f4d4eb1e3
Jian James Astrero <jianjamesastrero@gmail.com>
2018-10-01 06:02:20 +0800

    Logout Component

---
+++ src/resources/js/components/JuggerLogoutComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="align-middle text-center text-light bg-primary jugger-full-screen lineheight-100vh">
+        <i class="fas fa-spin fa-cog fa-10x"></i>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LogoutComponent',
+        props: [
+            'rootUrl'
+        ],
+        data() {
+            return {
+                name: 'LogoutComponent'
+            }
+        },
+        mounted() {
+            this.logout();
+        }
+    }
+</script>
+
+<style scoped>
+    .lineheight-100vh {
+        line-height: 100vh;
+    }
+</style>
