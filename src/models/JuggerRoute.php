63e4e14cad46e183f80e45f8ff9237c3d1ac6a27
Jian James Astrero <jianjamesastrero@gmail.com>
2018-10-01 05:58:55 +0800

    Created JuggerRoute Model

---
+++ src/models/JuggerRoute.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace JianAstrero\JuggerAPI\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use JianAstrero\JuggerAPI\Traits\CanMutate;
+use JianAstrero\JuggerAPI\Traits\HasTable;
+
+class JuggerRoute extends Model
+{
+    use HasTable, CanMutate;
+
+    protected $fillable = [
+        'model_name',
+        'slug',
+        'columns',
+        'column_override',
+        'sort',
+        'sort_override',
+        'create',
+        'read',
+        'update',
+        'delete',
+        'list',
+        'paginate_item_count'
+    ];
+
+    protected $casts = [
+        'columns' => 'array',
+        'sort' => 'array',
+    ];
+}
