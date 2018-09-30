62bc9c85c1c75a298c772e5698aa4c0d10d6d406
Jian James Astrero <jianjamesastrero@gmail.com>
2018-10-01 05:59:16 +0800

    Created Jugger Seed cli tool

---
+++ src/Commands/JuggerSeed.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use JianAstrero\JuggerAPI\Models\JuggerRoute;
+
+class JuggerSeed extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'jugger:seed';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Seed jugger routes with itself such that Jugger API frontend would work';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $juggerRoute = new JuggerRoute();
+        $juggerRoute->model_name = 'App\JuggerRoute';
+        $juggerRoute->slug = 'jugger-api-routes';
+        $juggerRoute->columns =
+            array(
+                'id',
+                'model_name',
+                'slug',
+                'columns',
+                'column_override',
+                'sort',
+                'sort_override',
+                'create',
+                'read',
+                'update',
+                'delete',
+                'list',
+                'paginate_item_count',
+            );
+        $juggerRoute->column_override = true;
+        $juggerRoute->sort = array('model_name','asc');
+        $juggerRoute->sort_override = true;
+        $juggerRoute->save();
+    }
+}
